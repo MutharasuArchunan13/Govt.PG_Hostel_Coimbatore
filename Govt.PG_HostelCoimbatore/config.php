93cb8c75a95240727e1eef96658a6afc4777042e
MutharasuArchunan13 <mutharasu39@gmail.com>
2023-08-31 18:10:51 +0530

    updated successfully

---
+++ Govt.PG_HostelCoimbatore/config.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = " ";
+$dbname = "registration";
+
+$conn=mysqli_connect("localhost","root"," ","registration") or die("unable to connect");
+?>
